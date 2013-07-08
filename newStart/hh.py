63e84232f982dab52d2baced6ac6980c4ed93627
wesleybowman <wesley.bowman23@gmail.com>
2013-07-08 13:05:30 -0300

    Vectorized for loop

--- newStart/hh.py
+++ newStart/hh.py
@@ -5,11 +5,6 @@ import time
 from numba import autojit
 from math import sqrt
 
-def X(Xprime,Yprime,Rprime,L):
-    return (Xprime*L)/Rprime
-
-def Y(Xprime,Yprime,Rprime,L):
-    return (Yprime*L)/Rprime
 
 @autojit
 def main():
@@ -26,8 +21,8 @@ def main():
 
     wavelength=405e-9
     k=2*np.pi/(wavelength)
-    L=250e-6
-    #z=13e-3
+    #L=250e-6
+    L=13e-3
     #z=13e-3-250e-6
 
     distX=6e-6
@@ -36,18 +31,41 @@ def main():
     n=float(img.shape[0])
     m=float(img.shape[1])
 
-    for x in xrange(img.shape[0]):
-        for y in xrange(img.shape[1]):
-            print(x,y)
-            r=sqrt(L*L+x*x+y*y)
-            Xprime=(x*L)/r
-            Yprime=(y*L)/r
-            Rprime=(L*L)/r
-            xx=X(Xprime,Yprime,Rprime,L)
-            yy=Y(Xprime,Yprime,Rprime,L)
+    first=time.time()
 
-            return img[xx,yy]
+    a=np.mgrid[0:n,0:m][0]
+    b=np.mgrid[0:n,0:m][1]
 
+    r=np.sqrt(L*L+a*a+b*b)
+    Xprime=(a*L)/r
+    Yprime=(b*L)/r
+    Rprime=(L*L)/r
+    xx=(Xprime*L)/Rprime
+    yy=(Yprime*L)/Rprime
+    xx=xx.astype(int)
+    yy=yy.astype(int)
+
+    print(img[xx,yy])
+
+    temp[xx,yy]=img[xx,yy]*(L/Rprime)**4*np.exp((1j*k*z*Rprime)/L)
+    print('temp')
+    print(temp)
+
+    print(time.time()-first)
+
+#    for x in xrange(img.shape[0]):
+#        for y in xrange(img.shape[1]):
+#            print(x,y)
+#            r=sqrt(L*L+x*x+y*y)
+#            Xprime=(x*L)/r
+#            Yprime=(y*L)/r
+#            Rprime=(L*L)/r
+#            xx=(Xprime*L)/Rprime
+#            yy=(Yprime*L)/Rprime
+#
+#            print(img[xx,yy])
+#
+#    print(time.time()-first)
 
 main()
 
