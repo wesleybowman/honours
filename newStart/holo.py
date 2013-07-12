46dd15f4694755a49ae2e03d8e93a2aa7d19ef97
wesleybowman <wesley.bowman23@gmail.com>
2013-07-12 14:36:27 -0300

    changes to holo

--- newStart/holo.py
+++ newStart/holo.py
@@ -3,27 +3,24 @@ import holopy as hp
 import numpy as np
 import matplotlib.pyplot as plt
 
-''' setting up optics '''
+# setting up optics
 optics = hp.core.Optics(wavelen=.405, index=1.33,
                         polarization=[1.0, 0.0])
 
-''' loading the images '''
-obj = hp.core.load('jerichoObject.bmp',spacing=6,optics=optics)
-ref = hp.core.load('jerichoRef.bmp',spacing=6,optics=optics)
+# loading the images
+obj = hp.load('jerichoObject.bmp',spacing=6,optics=optics)
+ref = hp.load('jerichoRef.bmp',spacing=6,optics=optics)
 
-''' contrast image '''
+# contrast image
 holo=obj-ref
 
-'''reconstruction, same image for all slices though '''
+# reconstruction, same image for all slices though
 #rec = hp.propagate(holo, 13e-3)
 #rec = hp.propagate(holo, 13e-3-250e-6)
-rec = hp.propagate(holo, np.linspace(245e-6,13e-3,20))
+rec = hp.propagate(holo, np.linspace(200,300,10))
 
-''' intensity so pyplot can plot it '''
-recInt=rec.real*rec.real+rec.imag*rec.imag
+# intensity so pyplot can plot it
+recInt=abs(rec)*abs(rec)
 
-''' hp.show doesn't show me anything,unless plt.show() is also present '''
 hp.show(recInt)
-
-''' how I have to view it '''
 plt.show()
