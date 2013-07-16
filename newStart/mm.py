f9892faed8f1ae3b87aa10c001be45c5a2f306f1
wesleybowman <wesley.bowman23@gmail.com>
2013-07-16 11:16:17 -0300

    more vectorizing

--- newStart/mm.py
+++ newStart/mm.py
@@ -25,27 +25,38 @@ z=250e-6
 distX=6e-6
 distY=6e-6
 
-n=float(holo.shape[0])
-m=float(holo.shape[1])
 n=holo.shape[0]
 m=holo.shape[1]
 
 #create all r vectors
-R = np.empty((holo.shape[0], holo.shape[1], 3))
-R[:,:,0] = np.repeat(np.arange(holo.shape[0]), holo.shape[1]).reshape(holo.shape) * distX
-R[:,:,1] = np.arange(holo.shape[1]) * distY
+R = np.empty((n, m, 3))
+R[:,:,0] = np.repeat(np.arange(n), m).reshape(holo.shape) * distX
+R[:,:,1] = np.arange(m) * distY
 R[:,:,2] = z
 
 #create all ksi vectors
-KSI = np.empty((holo.shape[0], holo.shape[1], 3))
-KSI[:,:,0] = np.repeat(np.arange(holo.shape[0]), holo.shape[1]).reshape(holo.shape) * distX
-KSI[:,:,1] = np.arange(holo.shape[1]) * distY
+KSI = np.empty((n, m, 3))
+KSI[:,:,0] = np.repeat(np.arange(n),m).reshape(holo.shape) * distX
+KSI[:,:,1] = np.arange(m) * distY
 KSI[:,:,2] = z
 
 # vectorized 2-norm; see http://stackoverflow.com/a/7741976/4323
 KSInorm = np.sum(np.abs(KSI)**2,axis=-1)**(1./2)
 
 KSIdotR=inner1d(KSI,R)
+''' need to be able to pick out one element and divide that element with matrix
+    KSInorm, for every element in KSIdotR all at once '''
+
+''' figured out that an ogrid is perfect for this is you ravel KSIdotR,
+    memory errors insued. '''
+
+KSIdotR=KSIdotR.ravel()
+x,_=np.ogrid[0:n,0:m]
+KSIdotR=KSIdotR[x]
+
+''' might work, check after lunch '''
+temp = ne.evaluate('holo * exp(1j * k * KSIdotR / KSInorm)')
+print(temp)
 
 # loop over entire holo which is same shape as holo, rows first
 # this loop populates holo one pixel at a time (so can be parallelized)
@@ -60,7 +71,7 @@ for x in rowsX:
 
         #KSIdotR = np.dot(KSI[x,y], R[x,y])
         #temp = ne.evaluate('holo * exp(1j * k * KSIdotR / KSInorm)')
-        #temp = holo * np.exp(1j*k*KSIdotR[x,y]/KSInorm)
+
         tempKSI=KSIdotR[x,y]
         temp = ne.evaluate('holo * exp(1j * k * tempKSI / KSInorm)')
 
