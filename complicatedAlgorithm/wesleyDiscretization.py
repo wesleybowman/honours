228495b33e048a1b1438bf6d95a48663a9470f4a
wesleybowman <wesley.bowman23@gmail.com>
2013-07-22 11:36:56 -0300

    cleaned up

--- complicatedAlgorithm/wesleyDiscretization.py
+++ complicatedAlgorithm/wesleyDiscretization.py
@@ -1,4 +1,3 @@
-''' Trying to vectorize code to make it faster '''
 from __future__ import division,print_function
 import numpy as np
 import matplotlib.pyplot as plt
@@ -39,12 +38,12 @@ first=time.time()
 
 a,b=np.mgrid[0:n,0:m]
 
-r=ne.evaluate('sqrt(L*L+a*a+b*b)')
-Xprime=ne.evaluate('(a*L)/r')
-Yprime=ne.evaluate('(b*L)/r')
-Rprime=ne.evaluate('(L*L)/r')
-xx=ne.evaluate('(Xprime*L)/Rprime')
-yy=ne.evaluate('(Yprime*L)/Rprime')
+r=ne.evaluate('sqrt(L*L + a*a + b*b)')
+Xprime=ne.evaluate('(a*L) / r')
+Yprime=ne.evaluate('(b*L) / r')
+Rprime=ne.evaluate('(L*L) / r')
+xx=ne.evaluate('(Xprime*L) / Rprime')
+yy=ne.evaluate('(Yprime*L) / Rprime')
 xx=xx.astype(int)
 yy=yy.astype(int)
 
@@ -59,16 +58,16 @@ yy=yy.astype(int)
 
 ''' z is the slice we want to look at '''
 z=250e-6
-z=13e-3-250e-6
+#z=13e-3-250e-6
 #z=13e-3
 
 print('Distance: {0}'.format(z))
 
-imgPrime=ne.evaluate('img*(L/Rprime)**4*exp((1j*k*z*Rprime)/L)')
+imgPrime=ne.evaluate('img * (L / Rprime)**4 * exp((1j * k * z * Rprime) / L)')
 
 K=np.fft.ifft2(imgPrime)
 
-Kint=abs(K)*abs(K)
+Kint=abs(K) * abs(K)
 
 plot(Kint)
 logPlot(Kint)
