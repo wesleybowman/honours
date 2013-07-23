02e25b99b62fe668e78eda7702a687469c876084
wesleybowman <wesley.bowman23@gmail.com>
2013-07-23 10:38:37 -0300

    testing

--- complicatedAlgorithm/wesleyDiscretization.py
+++ complicatedAlgorithm/wesleyDiscretization.py
@@ -15,7 +15,6 @@ obj=plt.imread('jerichoObject.bmp')
 ref=plt.imread('jerichoRef.bmp')
 
 img=obj-ref
-img=obj
 
 temp=np.empty(img.shape)+0j
 imgPrime=np.empty(img.shape)+0j
@@ -34,25 +33,41 @@ distY=6e-6
 
 n,m=img.shape
 
+#''' tested to make sure the vectorized code was the same as the for loops '''
+#tempXprime=np.empty(img.shape)
+#tempYprime=np.empty(img.shape)
+#tempR=np.empty(img.shape)
+#tempxx=np.empty(img.shape)
+#tempyy=np.empty(img.shape)
+#tempRprime=np.empty(img.shape)
+#
+#for x in xrange(n):
+#    for y in xrange(m):
+#        r=np.sqrt(L*L+x*x+y*y)
+#        tempR[x,y]=r
+#        tempRprime[x,y]=(L*L)/r
+#        tempXprime[x,y]=(x*L)/r
+#        tempYprime[x,y]=(y*L)/r
+#        tempxx[x,y]=(tempXprime[x,y]*L)/tempRprime[x,y]
+#        tempyy[x,y]=(tempYprime[x,y]*L)/tempRprime[x,y]
+#
+#tempxx=tempxx.astype(int)
+#tempyy=tempyy.astype(int)
+
 a,b=np.mgrid[0:n,0:m]
 
 r=ne.evaluate('sqrt(L*L + a*a + b*b)')
 Xprime=ne.evaluate('(a*L) / r')
 Yprime=ne.evaluate('(b*L) / r')
 Rprime=ne.evaluate('(L*L) / r')
+
+''' xx is the inverse transform of Xprime, refered to as X in the paper '''
 xx=ne.evaluate('(Xprime*L) / Rprime')
 yy=ne.evaluate('(Yprime*L) / Rprime')
 xx=xx.astype(int)
 yy=yy.astype(int)
 
-#r=np.sqrt(L*L+a*a+b*b)
-#Xprime=(a*L)/r
-#Yprime=(b*L)/r
-#Rprime=(L*L)/r
-#xx=(Xprime*L)/Rprime
-#yy=(Yprime*L)/Rprime
-#xx=xx.astype(int)
-#yy=yy.astype(int)
+#test=(tempxx==xx).all() #True
 
 ''' z is the slice we want to look at '''
 z=250e-6
@@ -61,12 +76,24 @@ z=13e-3-250e-6
 
 print('Distance: {0}'.format(z))
 
+#test=(img[xx,yy]==img).all() #This is False!
+#print(test)
+
 imgPrime=ne.evaluate('img * (L / Rprime)**4 * exp((1j * k * z * Rprime) / L)')
 
-#pi=np.pi
-#imgPrime=ne.evaluate('imgPrime*exp(-1j*pi*(xx*xx*distX*distX+yy*yy*distY*distY)/(wavelength*z))')
+newimg=img[xx,yy]
+NewimgPrime=ne.evaluate('newimg * (L / Rprime)**4 * exp((1j * k * z * Rprime) / L)')
+
+#test=(imgPrime==NewimgPrime).all() #False
+#bad=np.where(imgPrime!=NewimgPrime)
+#imgPrime[bad[0],bad[1]]
+#NewimgPrime[bad[0],bad[1]]
+
+# Not exactly the same, but close enough that it doesn't change the appearance
+# of the output image
 
 K=np.fft.ifft2(imgPrime)
+#K=np.fft.ifft2(NewimgPrime)
 
 Kint=abs(K) * abs(K)
 
