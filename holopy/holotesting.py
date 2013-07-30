b8b780d860c0a8d2ae2c02d86003f86db9d9e150
wesleybowman <wesley.bowman23@gmail.com>
2013-07-30 14:25:51 -0300

    forgot what changed

--- holopy/holotesting.py
+++ holopy/holotesting.py
@@ -8,13 +8,21 @@ def myHolo():
 
     optics = hp.core.Optics(wavelen=635e-9, index=1.33, polarization=[1.0, 0.0])
 
-    obj = hp.load('tryit.png', spacing=7.6e-6, optics=optics)
-    ref = hp.load('ref.png', spacing=7.6e-6, optics=optics)
+    #obj = hp.load('tryit.png', spacing=7.6e-6, optics=optics)
+    #ref = hp.load('ref.png', spacing=7.6e-6, optics=optics)
+
+    obj = hp.load('fibreBright.png', spacing=7.6e-6, optics=optics)
+    ref = hp.load('refBright.png', spacing=7.6e-6, optics=optics)
+
+    #obj = hp.load('singleBright.png', spacing=7.6e-6, optics=optics)
+    #ref = hp.load('singleRef.png', spacing=7.6e-6, optics=optics)
 
     holo = obj - ref
 
     ''' between 4 cm and 10 cm, should be about 5.5 cm '''
-    rec = hp.propagate(holo, np.linspace(4e-2, 10e-2, 200))
+    #rec = hp.propagate(holo, np.linspace(4e-2, 10e-2, 200))
+    rec = hp.propagate(holo, np.linspace(5e-2, 6e-2, 200))
+    #rec = hp.propagate(holo, np.linspace(0.5e-2, 2e-2, 200))
 
     return rec
 
@@ -37,10 +45,10 @@ hp.show(phase)
 #hp.show(np.angle(rec))
 plt.show()
 
-''' 3D plotting - useful? '''
-#from mayavi import mlab
-#mlab.contour3d(recInt)
-##    mlab.pipeline.volume(mlab.pipeline.scalar_field(newImg))
-#mlab.axes(x_axis_visibility=True,y_axis_visibility=True,z_axis_visibility=True)
-#mlab.outline()
-#mlab.show()
+#import os
+#os.chdir('saves/')
+#count=0
+#for i in recInt:
+#    filename = 'figure{}'.format(count)
+#    plt.imsave(filename, i)
+#    count+=1
