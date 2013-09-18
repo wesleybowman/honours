a7a243d9606cad6ce1b872ae32fcc401f96c3691
wesleybowman <wesley.bowman23@gmail.com>
2013-09-18 11:41:21 -0300

    fixin

--- holopy/experimental/h5test.py
+++ holopy/experimental/h5test.py
@@ -1,8 +1,8 @@
 from __future__ import division, print_function
 import holopy as hp
 import numpy as np
-import matplotlib.pyplot as plt
 import tables as tb
+from mpi4py import MPI
 
 def loadHolo():
     optics = hp.core.Optics(wavelen=.66, index=1.33, polarization=[1.0, 0.0])
@@ -19,16 +19,24 @@ def loadHolo():
 
 holo = loadHolo()
 
+comm=MPI.COMM_WORLD
+
 slices = np.arange(100,10100,100)
+n=100
+newSlices = [comm.rank + comm.size * aa for aa in range(int(n/comm.size)+1) if comm.rank + comm.size*aa < n]
 
-f = tb.openFile('P1819.hdf','w')
-matrix = f.createCArray(f.root, 'complexHologram',tb.ComplexAtom(itemsize=8), shape=(4016,3016,100))
+comm.Barrier()
 
-count = 0
-for i in slices:
 
-    print(i)
-    rec = hp.propagate(holo, i)
-    matrix[..., count] = rec
-    count += 1
+with tb.openFile('P1819.hdf','w') as f:
+    matrix = f.createCArray(f.root, 'complexHologram',tb.ComplexAtom(itemsize=8), shape=(4016,3016,100))
+
+    #count = 0
+    for i in newSlices:
+
+        print(i, slices[i])
+        rec = hp.propagate(holo, slices[i])
+        matrix[..., i] = rec
+        #matrix[..., count] = rec
+        #count += 1
 
